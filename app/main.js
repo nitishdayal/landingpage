2456fa7b34e5602d62f1be21c0ed1446ee167dba
Nitish Dayal <nitishdayal@outlook.com>
2016-11-16 13:57:39 -0800

    Added .gitignore file

---
+++ app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var header_module_1 = require("./header/header.module");
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(header_module_1.HeaderModule);
+//# sourceMappingURL=main.js.map
