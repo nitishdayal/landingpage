203f89c5e418f09cf80ddf48044f4c640249a7a4
Nitish Dayal <nitishdayal@outlook.com>
2016-11-22 05:55:39 -0800

    change all the things

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+/**
+ * @type {HTMLElement}
+ * @public
+ */
+
+var _prev_active;
+
+/**
+ * Sets 'active' class to appropriate link
+ * 
+ * @param {String} link_id    The 'id' attribute of the selected link
+ * @property {HTMLElement} prev_active 
+ */
+
+function setActive(link_id){
+
+  if (_prev_active != undefined){
+    _prev_active.classList.remove('active');
+  }
+
+  _prev_active = document.getElementById(link_id);
+  _prev_active.classList.add('active');
+
+}
