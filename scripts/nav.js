fb2fd1f85db3784c261837bcbd4ea447c35eef2b
Nitish Dayal <nitishdayal@outlook.com>
2016-11-23 20:49:38 -0800

    Cleanupcleanupcleanup

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
-  
+
    /**
    * Smooth scrolling for local anchor tags
    */
 
   $('a[href*="#"]:not([href="#"])').click(function(e) {
-    e.preventDefault()
-    if (location.pathname.replace(/^\//, '') === 
-      this.pathname.replace(/^\//, '') && location.hostname === 
-      this.hostname) 
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
+        && location.hostname === this.hostname) 
     {
+      var link_item = '.nav-links li a';
+
+      $('.nav-links li').removeClass('active');
+      $(link_item).removeClass('active');
+
+      $(this.parentElement).addClass('active');
+      $(this).addClass('active');
+
       var target = $(this.hash);
-      target = target.length ? target :
-        $('[name=' + this.hash.slice(1) + ']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
       if (target.length) {
         $('html, body').animate({scrollTop: target.offset().top}, 1200);
         return false;
