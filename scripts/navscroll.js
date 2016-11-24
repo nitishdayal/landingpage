db34f0797445340902a4d06f74f04a3fefcf4a5f
Nitish Dayal <nitishdayal@outlook.com>
2016-11-23 19:59:25 -0800

    forever fiddling with css

--- scripts/navscroll.js
+++ scripts/navscroll.js
@@ -1,33 +1,41 @@
 $(document).ready(function() {
-  /**
+  
+   /**
    * Smooth scrolling for local anchor tags
    */
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      && location.hostname == this.hostname
-    ) {
+
+  $('a[href*="#"]:not([href="#"])').click(function(e) {
+    e.preventDefault()
+    if (location.pathname.replace(/^\//, '') === 
+      this.pathname.replace(/^\//, '') && location.hostname === 
+      this.hostname) 
+    {
       var target = $(this.hash);
       target = target.length ? target :
         $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({scrollTop: target.offset().top}, 1000);
+        $('html, body').animate({scrollTop: target.offset().top}, 1200);
         return false;
       }
     }
+
   });
+
+
   /**
    * Stick navbar to top of page once user scrolls past header
    */
+
   $(window).scroll(function () {
     
-    if ($(window).scrollTop() > 600) {
+    if ($(window).scrollTop() > $('#title-block').height()) {
       $('#content').addClass('content-push');
       $('#nav-bar').addClass('navbar-fixed');
     }
-    if ($(window).scrollTop() < 601) {
+    if ($(window).scrollTop() < $('#title-block').height()) {
       $('#content').removeClass('content-push');
       $('#nav-bar').removeClass('navbar-fixed');
     }
   });
+
 });
