c32829e8393a35cca02bf7d2f6761fa8376cae98
Nitish Dayal <nitishdayal@outlook.com>
2016-11-23 21:00:58 -0800

    No more fiddling with the design. Promise.

--- scripts/nav.js
+++ scripts/nav.js
@@ -5,8 +5,12 @@ $(document).ready(function() {
    */
 
   $('a[href*="#"]:not([href="#"])').click(function(e) {
-    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
-        && location.hostname === this.hostname) 
+
+    e.preventDefault();
+
+    if (location.pathname.replace(/^\//, '') === 
+      this.pathname.replace(/^\//, '') && 
+      location.hostname === this.hostname) 
     {
       var link_item = '.nav-links li a';
 
